1164b223b3ad46880f1a0db5a8445ebe0a3e38ef
David <dgkilolo23@gmail.com>
2020-02-15 22:48:15 +0300

    adds form fields for date, month & year.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,18 @@ var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
+var DD = document.getElementById('date')
+var MM = document.getElementById('month')
+var YY = document.getElementById('year')
+
 var man = document.getElementById('male-checkbox')
 var woman = document.getElementById('female-checkbox')
 
 akan(); {
-  var answer = new Date().getDay()
+  var answer = new Date(DD+'/'+MM+'/'+YY).getDay()
 
-
-  
+  if (answer == 0 && man.checked) {
+    alert("You were born on " + days[0] + "," + "and your Akan name is " + male[0] + ".")
+  }
 
 }
