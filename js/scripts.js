d2a4cf5c27e7c40c20e3d6ef069c6e68302c1fc6
David <dgkilolo23@gmail.com>
2020-02-15 23:44:34 +0300

    adds changes to the spacing of the alert output.

--- js/scripts.js
+++ js/scripts.js
@@ -17,35 +17,35 @@ function akan() {
   //alert(name)
   
   if (name === 0 && woman.checked){
-    alert("You were born on " + days[0] + "," + "and your Akan name is " + female[0] + ".")
+    alert("You were born on " + days[0] + "," + " and your Akan name is " + female[0] + ".")
   } else if(name === 3 && woman.checked){
-    alert("You were born on " + days[0] + "," + "and your Akan name is " + female[0] + ".")
+    alert("You were born on " + days[0] + "," + " and your Akan name is " + female[0] + ".")
   } else if(name === 1 && woman.checked){
-    alert("You were born on " + days[0] + "," + "and your Akan name is " + female[0] + ".")
+    alert("You were born on " + days[0] + "," + " and your Akan name is " + female[0] + ".")
   } else if (name === 0 && woman.checked){
-    alert("You were born on " + days[0] + "," + "and your Akan name is " + female[0] + ".")
+    alert("You were born on " + days[0] + "," + " and your Akan name is " + female[0] + ".")
   } else if (name === 5 && woman.checked){
-    alert("You were born on " + days[0] + "," + "and your Akan name is " + female[0] + ".")
+    alert("You were born on " + days[0] + "," + " and your Akan name is " + female[0] + ".")
   } else if (name === 2 && woman.checked) {
-    alert("You were born on " + days[0] + "," + "and your Akan name is " + female[0] + ".")
+    alert("You were born on " + days[0] + "," + " and your Akan name is " + female[0] + ".")
   } else if (name === 6 && woman.checked){
-    alert("You were born on " + days[0] + "," + "and your Akan name is " + female[0] + ".")
+    alert("You were born on " + days[0] + "," + " and your Akan name is " + female[0] + ".")
   }
   
   if (name === 0 && man.checked){
-    alert("You were born on " + days[0] + "," + "and your Akan name is " + male[0] + ".")
+    alert("You were born on " + days[0] + "," + " and your Akan name is " + male[0] + ".")
   } else if(name === 1 && man.checked){
-    alert("You were born on " + days[1] + "," + "and your Akan name is " + male[1] + ".")
+    alert("You were born on " + days[1] + "," + " and your Akan name is " + male[1] + ".")
   } else if(name === 2 && man.checked){
-    alert("You were born on " + days[2] + "," + "and your Akan name is " + male[2] + ".")
+    alert("You were born on " + days[2] + "," + " and your Akan name is " + male[2] + ".")
   } else if (name === 3 && man.checked){
-    alert("You were born on " + days[3] + "," + "and your Akan name is " + male[3] + ".")
+    alert("You were born on " + days[3] + "," + " and your Akan name is " + male[3] + ".")
   } else if (name === 4 && man.checked){
-    alert("You were born on " + days[4] + "," + "and your Akan name is " + male[4] + ".")
+    alert("You were born on " + days[4] + "," + " and your Akan name is " + male[4] + ".")
   } else if (name === 5 && man.checked) {
-    alert("You were born on " + days[5] + "," + "and your Akan name is " + male[5] + ".")
+    alert("You were born on " + days[5] + "," + " and your Akan name is " + male[5] + ".")
   } else if (name === 6 && man.checked){
-    alert("You were born on " + days[6] + "," + "and your Akan name is " + male[6] + ".")
+    alert("You were born on " + days[6] + "," + " and your Akan name is " + male[6] + ".")
   }
   
   }
