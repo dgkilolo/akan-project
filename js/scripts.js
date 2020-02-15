4ea485518aa50522db1217a0d44fd380f7ac983c
David <dgkilolo23@gmail.com>
2020-02-15 22:54:14 +0300

    adds the if statments for the male parameter.

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,18 @@ akan(); {
 
   if (answer == 0 && man.checked) {
     alert("You were born on " + days[0] + "," + "and your Akan name is " + male[0] + ".")
+  } else if (answer == 1 && man.checked) {
+    alert("You were born on " + days[1] + "," + "and your Akan name is " + male[1] + ".")
+  } else if (answer == 2 && man.checked) {
+    alert("You were born on " + days[2] + "," + "and your Akan name is " + male[2] + ".")
+  } else if (answer == 3 && man.checked) {
+    alert("You were born on " + days[3] + "," + "and your Akan name is " + male[3] + ".")
+  } else if (answer == 4 && man.checked) {
+    alert("You were born on " + days[4] + "," + "and your Akan name is " + male[4] + ".")
+  } else if (answer == 5 && man.checked) {
+    alert("You were born on " + days[5] + "," + "and your Akan name is " + male[5] + ".")
+  } else if (answer == 6 && man.checked) {
+    alert("You were born on " + days[6] + "," + "and your Akan name is " + male[6] + ".")
   }
 
 }
