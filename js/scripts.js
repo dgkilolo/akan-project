aa76247bf7537b9c4fab0521693dde9329f0ea8d
David <dgkilolo23@gmail.com>
2020-02-15 22:18:01 +0300

    adds js and html code for the male/female check options.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
 var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+var man = document.getElementById('male-checkbox')
+var woman = document.getElementById('female-checkbox')
+
+akan(); {
+  var answer = new Date().getDay()
+
+
+  
+
+}
